0bd62444ed582e16f1ec177d248f5ce23af0ced4
Ch3did <phchedidk@gmail.com>
2022-06-17 20:54:21 -0300

    wip: update parser

--- p4j/parser.py
+++ p4j/parser.py
@@ -3,4 +3,19 @@
 
 class Parser:
     def __init__(self, string, template):
+        self.string = string
+        self.templates = template
+    
+    def decode(self):
+        # template to str
         pass
+    
+    def encode(self, template=None):
+        #str to template
+        if not self.template:
+            template = self.template
+        new_encode = {}
+        for item in self.template:
+            if type(item) ==  'dict':
+                self.encode(template=template[item], )
+    
