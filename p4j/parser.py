b4ef9822a3ec3bcc2851181c802ed2470e5bbdfd
Ch3did <phchedidk@gmail.com>
2022-06-21 01:46:18 -0300

    wip: make test coverage

--- p4j/parser.py
+++ p4j/parser.py
@@ -1,21 +1,47 @@
-
-
-
 class Parser:
     def __init__(self, string, template):
         self.string = string
-        self.templates = template
-    
+        self.template = template
+        self.new_encode = {}
+        
+    def _make_positional(self, template, item):
+        position_one = (
+            0
+            if not template[item][: template[item].find(":")]
+            else int(template[item][: template[item].find(":")])
+        )
+        position_two = (
+            0
+            if not template[item][template[item].find(":") + 1 :]
+            else int(template[item][template[item].find(":") + 1 :])
+        )
+        return self.string[position_one:position_two]
+        
+    def save_data(self):
+        pass   
+        
     def decode(self):
         # template to str
         pass
-    
+
     def encode(self, template=None):
-        #str to template
-        if not self.template:
-            template = self.template
-        new_encode = {}
-        for item in self.template:
-            if type(item) ==  'dict':
-                self.encode(template=template[item], )
-    
+        # str to template
+        template = self.template
+
+        def recurssive(template=None):
+            
+            recursive_encode = {}
+            for item in template:
+                if type(template[item]) == dict:
+                    recursive_encode[item] = recurssive(
+                        template=template[item],
+                    )
+                    self.new_encode.update(recursive_encode)
+                else:
+                    recursive_encode[item] = self._make_positional(template, item)
+            
+            return recursive_encode
+        
+        recurssive(template)
+        return self.new_encode
+
