5e6346e6fc80e5ff15b7768847b04bdf82336495
Ch3did <phchedidk@gmail.com>
2022-06-21 01:47:08 -0300

    ci: run black

--- p4j/template.py
+++ p4j/template.py
@@ -1,11 +1,11 @@
 from .utils import *
 
-class Template:
 
+class Template:
     @property
     def template(self):
         return self.__template
-    
+
     @property
     def __filename(self):
         return "p4j-config.json"
@@ -15,10 +15,7 @@ class Template:
             self.load_from_file()
         else:
             self.__template = kwargs
-    
+
     def load_from_file(self):
         self.__template = load_file(self.__filename)
         validate(self.__template)
-    
-        
-        
