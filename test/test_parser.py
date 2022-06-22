d1f23993d6d6e6342b668ed8fd4962f332633fc0
Ch3did <phchedidk@gmail.com>
2022-06-22 01:40:39 -0300

    wip: update test

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,21 +1,17 @@
-from tkinter import W
-import pytest
 from p4j.parser import Parser
 
 
 def test_parser(template, positional_string):
-    template
-
     p = Parser(string=positional_string, template=template).encode()
 
-    assert p.data['a'] == 'abc'
-    assert p.data['b'] == 'defgh'
-    # assert p.data['c'] == {
-    #     'x': 'ij',
-    #     'y': 'kl',
-    #     'z': {
-    #         "w": 'mnopqrstuvx',
-    #         "u": 'yw', 
-    #         "v": ''
-    #     }
-    # }
+    assert p.data["a"] == "abc"
+    assert p.data["b"] == "defgh"
+    assert p.data['c'] == {
+        'x': 'ij',
+        'y': 'kl',
+        'z': {
+            "w": 'mnopqrstuvx',
+            "u": 'yw',
+            "v": ''
+        }
+    }
